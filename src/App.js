fe9d88cce3c4f56b5f20b9805826fa8da02233a8
spyros zaboukis <spyros.zaboukis.83@gmail.com>
2023-02-01 13:04:19 +0100

    send routing system with react-router-v6

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './Pages/Home';
+import About from './Pages/About';
 
 function App() {
   return (
     <div className="App">
-      <p>Hello World</p>
+      
+      <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/About' element={<About/>} />
+            <Route path='*' element={<Home/>} />
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
---
+++ src/Pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navigation from '../Components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+             <Navigation/>
+            <p>
+                About me ... 
+            </p>
+        </div>
+    );
+};
+
+export default About;
