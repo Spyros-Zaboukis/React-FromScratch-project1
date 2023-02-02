4024c9f317f8093e37b007639e8897db32054731
spyros zaboukis <spyros.zaboukis.83@gmail.com>
2023-02-02 21:28:53 +0100

    send axios and call api to countries

--- src/Components/Countries.js
+++ src/Components/Countries.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Countries = () => {
+    const [data,setData] = useState([]);
+
+
+    useEffect( ()=> {
+        axios.get("https://restcountries.com/v3.1/all")
+        .then((res) => setData(res.data))
+    }, []
+    )
+
     return (
         <div className='countries'>
             <h1>Countries</h1>
