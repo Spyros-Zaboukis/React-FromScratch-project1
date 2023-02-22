9790cc40ed3f3f334d402b9778adb62ee433e8b7
spyros zaboukis <spyros.zaboukis.83@gmail.com>
2023-02-22 21:24:32 +0100

    send error style of Form of Blog page

--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from '../Components/Logo';
 import Navigation from '../Components/Navigation';
 
 const Blog = () => {
+
+    const [content , setContent] = useState("");
+    const [error , setError] = useState(false);
+
+   const handleSubmit = (e) => {
+    e.preventDefault();
+    if(content.length < 140 ){
+        setError(true);
+    }else{
+        setError(false);
+    }
+   }
+
     return (
         <div className='blog-container'>
             <Logo/>
@@ -10,14 +23,19 @@ const Blog = () => {
 
             <h1>Blog</h1>
 
-            <form>
+            <form onSubmit={(e)=>{handleSubmit(e)}}>
                 <input type='text' placeholder='Nom'/>
-                <textarea placeholder='Message'></textarea>
+                <textarea placeholder='Message' 
+                    onChangeCapture={(e) => setContent(e.target.value)}
+                    style={{ border : error ? "1px solid red" : "1px solid black" }}>
+
+                </textarea>
+                { error && <p>Erreur, vous devez écrire un minimum de 140 caractères</p> }
                 <input type='submit' value='Envoyer'/>
             </form>
             
         </div>
     );
-};
+}; 
 
 export default Blog;
